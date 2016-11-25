3be69352263eb78531358188563551a8dd821036
Minh Hiep <hiep24892@gmail.com>
2016-11-25 17:56:36 +0700

    delete question
    Conflicts:
    	app/Repositories/Eloquent/QuestionRepository.php
    	app/Repositories/Eloquent/UserRepository.php
    	resources/lang/en/messages.php

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -5,10 +5,12 @@ return [
     'errors' => [
         'create' => 'Failed to create a record in database due to a system error.',
         'update' => 'Failed to update a record in database due to a system error.',
+        'delete' => 'Failed to delete a record in database due to a system error.',
     ],
     'success' => [
         'create' => 'Created successfully!',
         'update' => 'Updated successfully!',
+        'delete' => 'Delete successfully!',
     ],
     'empty' => [
         'update' => 'You tried to update unempty question',
